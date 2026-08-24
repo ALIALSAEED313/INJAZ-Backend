9d92e5f48b7706deb268486656397cf8198f1064
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 10:41:50 +0300

    added getservicebyid

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -25,6 +25,23 @@ const getServices = async (req, res) => {
     });
   }
 };
+const getServiceById = async (req, res) => {
+  try {
+    const service = await Service.findById(req.params.id);
+
+    if (!service) {
+      return res.status(404).json({
+        message: "Service not found",
+      });
+    }
+    res.status(200).json(service);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to get service ",
+      error: error.message,
+    });
+  }
+};
 
 module.exports = {
   createService,
