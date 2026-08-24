f3a5908bd9c1e5509aeb499ae68cdea5b0799bd8
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 10:38:19 +0300

    added getservice

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,4 +1,4 @@
-const Service = require("../models/Service");
+const Service = require("../models/Services");
 
 const createService = async (req, res) => {
   try {
@@ -8,6 +8,19 @@ const createService = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       message: "Failed to create service",
+      error: error.message,
+    });
+  }
+};
+
+const getServices = async (req, res) => {
+  try {
+    const services = await Service.find();
+
+    res.status(200).json(services);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to get services",
       error: error.message,
     });
   }
