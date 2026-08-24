45751785df6efdb1f2ddc5ae29d56db75778150b
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-08-24 10:35:48 +0300

    added createservice

---
+++ controllers/service.controller.js
@@ -0,0 +1,18 @@
+const Service = require("../models/Service");
+
+const createService = async (req, res) => {
+  try {
+    const service = await Service.create(req.body);
+
+    res.status(201).json(service);
+  } catch (error) {
+    res.status(400).json({
+      message: "Failed to create service",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createService,
+};
