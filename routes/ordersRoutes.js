49b5ca460a05fe6bb2cba280cf4043dc29921410
ALIALSAEED313 <202300573@student.polytechnic.bh>
2026-08-26 11:51:27 +0300

    added get order by Id

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {createOrder, getUserOrders, updateOrderStatus} = require('../controllers/ordersController')
+const {createOrder, getUserOrders, updateOrderStatus, getOrderById} = require('../controllers/ordersController')
 
 const verifyToken = require("../middleware/verifyToken")
 
 router.use(verifyToken)
 router.post('/', createOrder)
 router.get('/my-orders', getUserOrders)
+router.get('/:id', getOrderById)
 router.put('/:id/status', updateOrderStatus)
 
 module.exports = router
